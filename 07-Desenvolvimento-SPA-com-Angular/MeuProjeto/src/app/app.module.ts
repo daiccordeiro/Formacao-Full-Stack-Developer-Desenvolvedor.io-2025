23c9e80192d4ebe818521921bffc84d70d6b7a7f
Daiane Cordeiro <redesdaiane@gmail.com>
2025-08-25 10:31:23 -0300

    Consumindo dados do Backend
    Utilizando Fake Backend com lista de produtos.json, utilizando HttpClient - 25/08/2025

--- 07-Desenvolvimento-SPA-com-Angular/MeuProjeto/src/app/app.module.ts
+++ 07-Desenvolvimento-SPA-com-Angular/MeuProjeto/src/app/app.module.ts
@@ -12,6 +12,9 @@ import { SobreComponent } from './institucional/sobre/sobre.component';
 import { ContatoComponent } from './institucional/contato/contato.component';
 import { rootRouterConfig } from './app.routes';
 import { DataBindingComponent } from './demos/data-binding/data-binding.component';
+import { ProdutoService } from './produtos/produtos.service';
+import { ListaProdutoComponent } from './produtos/lista-produto/lista-produto.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,14 +24,17 @@ import { DataBindingComponent } from './demos/data-binding/data-binding.componen
     FooterComponent,
     SobreComponent,
     ContatoComponent,
-    DataBindingComponent
+    DataBindingComponent,
+    ListaProdutoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
     [RouterModule.forRoot(rootRouterConfig, {useHash: false})]
   ],
   providers: [
+    ProdutoService,
     {provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
