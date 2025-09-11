0249e27d5e66a05bdad514b74b5f80c706ade464
Daiane Cordeiro <redesdaiane@gmail.com>
2025-09-11 08:51:14 -0300

    FormBuilder e Objetos Tipados
    Usando FormBuilder e transformando o formulário em Objetos tipados 11/09/25

--- 08-Desenvolvimento-Avançado-em-Angular/FORMS/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
+++ 08-Desenvolvimento-Avançado-em-Angular/FORMS/src/app/demos/reactiveForms/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { Usuario } from './models/usuario';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,21 +10,40 @@ import { FormControl, FormGroup } from '@angular/forms';
 export class CadastroComponent implements OnInit{
 
   cadastroForm: FormGroup;
+  usuario: Usuario;
+
   
-  constructor() { } 
+  // Usando FormGroup e FormControl
+  /*   
+   constructor() { } 
 
+   ngOnInit() {
+      this.cadastroForm = new FormGroup({
+        nome: new FormControl(''),
+        cpf: new FormControl(''),
+        email: new FormControl(''),
+        senha: new FormControl(''),
+        senhaConfirmacao: new FormControl(''),
+      });
+
+    }
+  */
+
+  // Usando FormBuilder
+  constructor(private fb: FormBuilder) {}
+  
   ngOnInit() {
-    this.cadastroForm = new FormGroup({
-      nome: new FormControl(''),
-      cpf: new FormControl(''),
-      email: new FormControl(''),
-      senha: new FormControl(''),
-      senhaConfirmacao: new FormControl(''),
+    this.cadastroForm = this.fb.group({
+      nome: [''],
+      cpf: [''],
+      email: [''],
+      senha: [''],
+      senhaConfirmacao: [''],
     });
-
   }
-
+  
+  // Tipando o objeto usuario, com a Model Usuario
   adicionarUsuario() {
-    let x = this.cadastroForm.value;
+    this.usuario = Object.assign({}, this.usuario, this.cadastroForm.value);
   }
 }
